62d1bc55244f832ddcc3d48d024fd68d903825d8
soniyanaik1177 <soniyanaik1177@gmail.com>
2023-11-05 17:49:02 -0600

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -27,11 +27,12 @@ export default class Footer extends Component {
 
     return (
       <a
+        href="#/"
         className={classnames({ selected: filter === selectedFilter })}
         style={{ cursor: "pointer" }}
         onClick={() => onShow(filter)}
       >
-        {title}
+        This is an example
       </a>
     );
   }
